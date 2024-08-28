1224179060b3b11aa932d2153b37e59daee9c007
萩萩萩 <qqm2017@163.com>
2024-08-28 15:05:06 +0800

    更新仓库地址

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from 'vuepress';
 import theme from './theme.js';
 
 export default defineUserConfig({
-    base: '/',
+    base: '/crashle.github.io/blog/',
 
     lang: 'zh-CN',
     title: '萩萩萩🌴的小站',
