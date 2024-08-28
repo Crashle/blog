9a55be7704f51ee6e52ee23eec1827154bc986d2
萩萩萩 <qqm2017@163.com>
2024-08-28 15:45:33 +0800

    更新base路径

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -4,7 +4,6 @@ import theme from './theme.js';
 
 export default defineUserConfig({
     base: '/blog/',
-
     lang: 'zh-CN',
     title: '萩萩萩🌴的小站',
     description: '即使慢，驰而不息，纵会落后，纵会失败，但必须能够到达他所向的目标。',
