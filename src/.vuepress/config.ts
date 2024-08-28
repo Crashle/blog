d84d7642fbe471da46b7f53db76018bd7a235d33
萩萩萩 <qqm2017@163.com>
2024-08-28 15:13:13 +0800

    更新资源路径

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from 'vuepress';
 import theme from './theme.js';
 
 export default defineUserConfig({
-    base: '/crashle.github.io/blog/',
+    base: '/blog/',
 
     lang: 'zh-CN',
     title: '萩萩萩🌴的小站',
